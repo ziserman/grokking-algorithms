25d9b9a378e6c3397da5226f09bee04e4aba6199
Danil Ziserman <d.ziserman@semrush.com>
2020-08-11 13:40:36 +0700

    feat: add "same numbers" test case for selectionSort

--- __tests__/selectionSort.test.ts
+++ __tests__/selectionSort.test.ts
@@ -7,3 +7,7 @@ test('Sort array with positive numbers', () => {
 test('Sort array with negative numbers and zero', () => {
     expect(selectionSort([-1, 200, 4005, -75211, 0, 6720])).toEqual([-75211, -1, 0, 200, 4005, 6720]);
 });
+
+test('Sort array with same numbers', () => {
+    expect(selectionSort([-1, 2, 3, -3, 2, 5])).toEqual([-3, -1, 2, 2, 3, 5]);
+});
